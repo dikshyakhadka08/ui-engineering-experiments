efb234bd5dc9779048250bc95c4970ba294102b9
Dikshya Khadka <user@Dikshyas-Macbook-Air-M4.local>
2026-07-27 11:53:30 +0545

    test

---
+++ cipher-project/CipherProject.py
@@ -0,0 +1,12 @@
+# Cipher Project
+#**What I learned:**
+#- String manipulation
+#- Loops (for, while)
+#- Indexing and slicing
+
+ #Index Learning
+
+name = "DikshyaKhadkaisUI/UXDesigner"
+print(name.index("X"))
+
+
